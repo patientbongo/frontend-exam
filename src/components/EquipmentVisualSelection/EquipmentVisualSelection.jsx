41b8810a33967aafd25dc4463b207256a24a2bcd
Eric Dulcet <etdulcet@gmail.com>
2020-06-11 09:02:31 -0400

    Improve EquipmentVisualSelection styling

--- src/components/EquipmentVisualSelection/EquipmentVisualSelection.jsx
+++ src/components/EquipmentVisualSelection/EquipmentVisualSelection.jsx
@@ -6,15 +6,18 @@ import { styles } from "./EquipmentVisualSelection.styles";
 export const EquipmentVisualSelection = () => {
   return (
     <div style={styles.parent}>
-      {mockData.map((equipment) => {
-        return (
-          <EquipmentPiece
-            image={equipment.image}
-            title={equipment.title}
-            key={equipment.id}
-          />
-        );
-      })}
+      <h4 style={styles.title}>Interested in our exciting iFit-enabled equipment?</h4>
+      <div style={styles.equipmentList}>
+        {mockData.map((equipment) => {
+          return (
+            <EquipmentPiece
+              image={equipment.image}
+              title={equipment.title}
+              key={equipment.id}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/components/EquipmentVisualSelection/EquipmentVisualSelection.styles.js
+++ src/components/EquipmentVisualSelection/EquipmentVisualSelection.styles.js
@@ -1,7 +1,21 @@
+import { colors, fontSizes } from "../Shared/sharedStyles";
+
 export const styles = {
   parent: {
     display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    margin: "15px 0",
+    letterSpacing: ".5px",
+    fontSize: fontSizes.large,
+    color: colors.lightGreyTransparent,
+  },
+  equipmentList: {
+    display: "flex",
     justifyContent: "space-between",
-    margin: "30px 20px",
+    margin: "10px 20px",
   },
 };
